1e6b4ebb54da987debc8f7fa4c66d8569c82b7d9
Kinjal Patel <kinju011@gmail.com>
2022-06-16 17:36:06 +0530

    certificate updated

--- examples/echo/server.rb
+++ examples/echo/server.rb
@@ -10,10 +10,30 @@ require 'async/io/host_endpoint'
 require 'async/io/stream'
 require 'certificate_authority'
 
+key = OpenSSL::PKey::RSA.new(2048)
+certificate_name = OpenSSL::X509::Name.parse("O=Test/CN=localhost")
+
+valid_certificate = begin
+	certificate = OpenSSL::X509::Certificate.new
+	certificate.subject = certificate_name
+	certificate.issuer = CertificateAuthority::DEFAULT.subject
+	certificate.public_key = key.public_key
+	certificate.serial = 2
+	certificate.version = 2
+	certificate.not_before = Time.now
+	certificate.not_after = Time.now + 3600
+	extension_factory = OpenSSL::X509::ExtensionFactory.new()
+	extension_factory.subject_certificate = certificate
+	extension_factory.issuer_certificate = CertificateAuthority::DEFAULT
+	certificate.add_extension extension_factory.create_extension("keyUsage", "digitalSignature", true)
+	certificate.add_extension extension_factory.create_extension("subjectKeyIdentifier", "hash")
+	certificate.sign CertificateAuthority::DEFAULT.certificate_authority_key, OpenSSL::Digest::SHA256.new
+end
+
 server_context =
   OpenSSL::SSL::SSLContext.new.tap do |context|
-    context.key = CertificateAuthority::DEFAULT.certificate_authority_key
-    context.cert = CertificateAuthority::DEFAULT
+    context.key = key
+    context.cert = valid_certificate
   end
 
 endpoint = Async::IO::Endpoint.tcp('localhost', 4578)
